7f5af5ec5c336b96278e01421a1c3224a09da6fc
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-11 19:11:58 +0300

    Update ServiceCollectionExtensions.cs

--- E.CProject/Extensions/ServiceCollectionExtensions.cs
+++ E.CProject/Extensions/ServiceCollectionExtensions.cs
@@ -7,14 +7,30 @@ public static class ServiceCollectionExtensions
         // Register poster-specific repositories
         services.AddScoped<IPosterRepository, PosterRepository>();
         services.AddScoped<IPosterCategoryRepository, PosterCategoryRepository>();
+        services.AddScoped<IPosterUserRepository, PosterUserRepository>();
 
         // Register poster-specific services
         services.AddScoped<PosterService>();
         services.AddScoped<PosterCategoryService>();
+        services.AddScoped<PosterUserService>();
 
         // Register AutoMapper profile
         services.AddAutoMapper(typeof(PosterMappingProfile));
 
+        // Register additional services
+        services.AddScoped<IPosterImageService, PosterImageService>();
+
         return services;
+    }
+
+    public static IServiceCollection AddPosterModuleWithSeparateDb(
+        this IServiceCollection services, 
+        string connectionString)
+    {
+        // Use this if you want a separate database for posters
+        services.AddDbContext<PosterDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services.AddPosterModule();
     }
 }
