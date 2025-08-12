d0e06986d82efe0c8e9fe172974e04a1d496918f
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-12 23:52:47 +0300

    Create GEHM.cs

---
+++ PosterAdmin/Middleware/GEHM.cs
@@ -0,0 +1,71 @@
+using System.Net;
+using System.Text.Json;
+
+namespace PosterAdmin.Middleware
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception occurred");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            
+            var response = new
+            {
+                error = new
+                {
+                    message = exception.Message,
+                    details = exception.InnerException?.Message
+                }
+            };
+
+            switch (exception)
+            {
+                case ArgumentException:
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case KeyNotFoundException:
+                    context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case UnauthorizedAccessException:
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                default:
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    response = new
+                    {
+                        error = new
+                        {
+                            message = "An internal server error occurred",
+                            details = "Please try again later"
+                        }
+                    };
+                    break;
+            }
+
+            var jsonResponse = JsonSerializer.Serialize(response);
+            await context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
